3069da2a3bdb03ccb2ae2491f706b129a908aafd
Michael A. Sinclair <squattingmonk@gmail.com>
2022-09-13 09:16:12 -0500

    Add additional operators to highlights
    This adds several missing operators that were not highlighted previously.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,68 +1,96 @@
-"if" @keyword
-"else" @keyword
-"do" @keyword
-"for" @keyword
-"while" @keyword
-"break" @keyword
-"continue" @keyword
-"return" @keyword
-"switch" @keyword
-"case" @keyword
-"default" @keyword
+(identifier) @variable
+
+[
+  "if"
+  "else"
+  "switch"
+  "case"
+  "default"
+] @conditional
+
+[
+  "do"
+  "for"
+  "while"
+  "break"
+  "continue"
+] @repeat
+
+"return" @keyword.return
+"#include" @include
+
 "struct" @type.builtin
-"#include" @keyword
 
-"." @operator
-"--" @operator
-"-" @operator
-"-=" @operator
-"=" @operator
-"!=" @operator
-"*" @operator
-"&" @operator
-"&&" @operator
-"+" @operator
-"++" @operator
-"+=" @operator
-"<" @operator
-"=" @operator
-"==" @operator
-">" @operator
-"||" @operator
+[
+  "."
+  "--"
+  "-"
+  "-="
+  "="
+  "!="
+  "%"
+  "%="
+  "*"
+  "*="
+  "/"
+  "/="
+  "&"
+  "&&"
+  "&="
+  "+"
+  "++"
+  "+="
+  "<"
+  "<<"
+  "<="
+  "<<="
+  "="
+  "=="
+  ">"
+  ">>"
+  ">>>"
+  ">="
+  ">>="
+  ">>>="
+  "|"
+  "||"
+  "|="
+  "^"
+  "^="
+  "~"
+] @operator
 
-";" @punctuation
-"{" @punctuation
-"}" @punctuation
-"[" @punctuation
-"]" @punctuation
-"(" @punctuation
-")" @punctuation
-"," @punctuation
+[ "." ";" "," ] @punctuation.delimiter
+
+(conditional_expression [ "?" ":" ] @conditional)
+
+
+[ "(" ")" "[" "]" "{" "}"] @punctuation.bracket
 
 
 (field_expression) @enum
 
 (const_qualifier) @type.modification
 (function_definition
-    declarator: (identifier) @function)
+  declarator: (identifier) @function)
 
 (struct_declarator
-    declarator: (identifier) @type)
+  declarator: (identifier) @type)
 (struct_declarator
-    (identifier) @type)
+  (identifier) @type)
 
-(struct_members 
-    (type_identifier) @type.builtin
-    (identifier) @enum)
+(struct_members
+  (type_identifier) @type.builtin
+  (identifier) @enum)
 
 (decoration) @keyword
 
 (struct_specifier
-    (identifier) @type)
+  (identifier) @type)
 (struct_specifier) @type
 
 (call_expression
-    function: (identifier) @function)
+  function: (identifier) @function)
 
 (primitive_type) @type.builtin
 (nwn_type) @type.builtin
@@ -75,11 +103,8 @@
 (nwn_constant) @constant.builtin
 
 ((identifier) @constant
-    (#match? @constant "^[A-Z][A-Z\\d_]*$"))
-(identifier) @variable
+  (#match? @constant "^[A-Z][A-Z\\d_]*$"))
 
 (nwnsc_macro) @macro
 
 (comment) @comment
-
-
